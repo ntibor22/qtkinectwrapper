f74785ae8882e83fba22ac0f6e79e7132a70e686
droggen <droggen@823da94b-fb53-b1e2-f3e2-113705b060a9>
2012-03-15 16:47:11 +0000

    minor stuff

--- packagewin.bat
+++ packagewin.bat
@@ -7,8 +7,10 @@ copy C:\QtSDK\Desktop\Qt\4.8.0\mingw\bin\QtGui4.dll bin
 copy C:\QtSDK\Desktop\Qt\4.8.0\mingw\bin\QtCore4.dll bin
 copy C:\QtSDK\Desktop\Qt\4.8.0\mingw\bin\QtNetwork4.dll bin
 copy "C:\Program Files (x86)\OpenNI\Bin\openni.dll" bin
-copy example\KinectReader-build-desktop\release\KinectReader.exe bin
-copy example\KinectLogger-build-desktop\release\KinectLogger.exe bin
+
+copy example\KinectReader-build-desktop-Qt_4_8_0_for_Desktop_-_MinGW__Qt_SDK__Release\release\KinectReader.exe bin
+copy example\KinectLogger-build-desktop-Qt_4_8_0_for_Desktop_-_MinGW__Qt_SDK__Release\release\KinectLogger.exe bin
+
 copy ..\qtffmpegwrapper_trunk\ffmpeg_lib_win32\*.dll bin
 
 cd bin
