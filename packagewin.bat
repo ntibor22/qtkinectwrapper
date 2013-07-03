992755946c75ba7ada5c3e3ce7ec01fca2674d5d
droggen <droggen@823da94b-fb53-b1e2-f3e2-113705b060a9>
2013-07-03 13:59:13 +0000

    Updated packaging scripts

--- packagewin.bat
+++ packagewin.bat
@@ -1,16 +1,26 @@
 @echo off
 rd /s /q bin
 md bin
-copy C:\QtSDK\Desktop\Qt\4.8.0\mingw\bin\libgcc_s_dw2-1.dll bin
-copy C:\QtSDK\Desktop\Qt\4.8.0\mingw\bin\mingwm10.dll bin
-copy C:\QtSDK\Desktop\Qt\4.8.0\mingw\bin\QtGui4.dll bin
-copy C:\QtSDK\Desktop\Qt\4.8.0\mingw\bin\QtCore4.dll bin
-copy C:\QtSDK\Desktop\Qt\4.8.0\mingw\bin\QtNetwork4.dll bin
+
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\libgcc_s_sjlj-1.dll bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\libwinpthread-1.dll bin
+copy "C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\libstdc++-6.dll" bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\icuin49.dll bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\icuuc49.dll bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\icudt49.dll bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\Qt5Gui.dll bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\Qt5Widgets.dll bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\Qt5Core.dll bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\Qt5Network.dll bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\libGLESv2.dll bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\D3DCompiler_43.dll bin
+copy C:\Qt\Qt5.0.2\5.0.2\mingw47_32\bin\libEGL.dll bin
+
 copy "C:\Program Files (x86)\OpenNI\Bin\openni.dll" bin
 copy "C:\Program Files\OpenNI\Bin\openni.dll" bin
 
-copy example\KinectReader-build-desktop\release\KinectReader.exe bin
-copy example\KinectLogger-build-desktop\release\KinectLogger.exe bin
+copy example\build-KinectReader-desktop\release\KinectReader.exe bin
+copy example\build-KinectLogger-desktop\release\KinectLogger.exe bin
 copy README bin
 
 copy ..\qtffmpegwrapper_trunk\ffmpeg_lib_win32\*.dll bin
